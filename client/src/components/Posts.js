dc6dff5390b63a3d2c38c4dc1f05dca46e2f0c00
SlimeVidas <dahan.isaac@gmail.com>
2021-09-02 23:13:33 +0300

    post-component

---
+++ client/src/components/Posts.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+import {React , useState , useEffect} from 'react'
+
+export default function Posts() {
+ 
+const [post, setpost] = useState(null)
+
+useEffect(() => {
+    axios.get('/posts')
+    .then((response)=> setpost(response.data))
+    .then((res)=> console.log(res))
+}, [])
+
+
+    return (
+        <div>
+           {JSON.stringify(post)}
+        </div>
+    )
+}
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT  = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(cors())
 
 app.get('/' , ((req , res)=>{
 res.send("<h1>Welcome to home page</h1>")
